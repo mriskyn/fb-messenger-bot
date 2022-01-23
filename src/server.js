7f6946533d25e36434939563390f5e1bcbc61044
Risky Nugraha <muhammadrisky83@gmail.com>
2022-01-23 18:42:47 +0700

    Body Parser Middleware

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const viwEngine = require('./config/viewEngine');
 const initWebRoutes = require('./routes/web');
@@ -7,6 +8,10 @@ const PORT = process.env.PORT || 8888;
 
 // config view engine
 viwEngine(app);
+
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // init all web routes
 initWebRoutes(app);
