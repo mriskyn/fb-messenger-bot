fb8007eec864db135935cbc741aad958ff64ac6a
Risky Nugraha <muhammadrisky83@gmail.com>
2022-01-23 23:18:39 +0700

    Add New Column

--- src/models/user.js
+++ src/models/user.js
@@ -2,18 +2,14 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      User.hasMany(models.Message)
+      User.hasMany(models.Message);
     }
   }
   User.init(
     {
       name: { type: DataTypes.STRING },
+      fbId: { type: DataTypes.STRING },
     },
     {
       sequelize,
