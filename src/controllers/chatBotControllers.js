24a4a404a5c4f162708de6598dd3130daaca1912
Risky Nugraha <muhammadrisky83@gmail.com>
2022-01-24 21:07:51 +0700

    debug

--- src/controllers/chatBotControllers.js
+++ src/controllers/chatBotControllers.js
@@ -13,7 +13,7 @@ const postWebHook = (req, res) => {
     body.entry.forEach(function (entry) {
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
-      console.log('webhook_event:', webhook_event.message);
+      // console.log('webhook_event:', webhook_event.message);
 
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
