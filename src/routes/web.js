ccbfc8a1253d293202bed45bcfc164e6a5fbacb9
Risky Nugraha <muhammadrisky83@gmail.com>
2022-01-25 01:33:12 +0700

    Add New Routes

--- src/routes/web.js
+++ src/routes/web.js
@@ -3,11 +3,16 @@ const router = express.Router();
 
 const homepageController = require('../controllers/homepageControllers');
 const chatBotController = require('../controllers/chatBotControllers');
+const messageController = require('../controllers/mesageController');
 
 const initWebRoutes = (app) => {
   router.get('/', homepageController.getHomepage);
   router.get('/webhook', chatBotController.getWebHook);
   router.post('/webhook', chatBotController.postWebHook);
+
+  router.get('/api/message', messageController.getMessages);
+  router.get('/api/message/:id', messageController.getMessage);
+  router.get('/api/summary', messageController.summary);
 
   return app.use('/', router);
 };
