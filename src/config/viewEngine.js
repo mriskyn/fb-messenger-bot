b624a0fa4cff94c3e7a5fa34fc263d67a6e804b4
Risky Nugraha <muhammadrisky83@gmail.com>
2022-01-25 09:19:08 +0700

    setup UI API Documentation

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 let configViewEngine = (app) => {
-  app.use(express.static('./src/public'));
   app.set('view engine', 'ejs');
   app.set('views', './src/views');
 };
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+
 const viwEngine = require('./config/viewEngine');
 const { sequelize } = require('./models');
 const initWebRoutes = require('./routes/web');
+const swaggerDocs = require('./docs');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
@@ -14,15 +17,21 @@ viwEngine(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// API Documentation (using :8888 port)
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocs, { customSiteTitle: 'API Documentation' })
+);
+
 // init all web routes
 initWebRoutes(app);
 
 async function main() {
   try {
     await sequelize.sync({ alter: true });
-    
   } catch (err) {
-    console.log('err:',err)
+    console.log('err:', err);
   }
   app.listen(PORT, () => console.log(`App is running on port:`, PORT));
 }
